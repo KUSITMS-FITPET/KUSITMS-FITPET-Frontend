191089232708b20ab4b0a8afa49c19fdfbbbf195
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-08-29 02:32:23 +0900

    style: 폰트 설정

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+const Pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={Pretendard.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
