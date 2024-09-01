c65ad77566717b65d3b253f66a9fef10964669a5
karnelll <165611407+karnelll@users.noreply.github.com>
2024-09-02 07:48:54 +0900

    보험팁 링크연결 + 충돌 수정 + merge 테스트 (이슈 #7)

--- fe/src/pages/components/common/Header.tsx
+++ fe/src/pages/components/common/Header.tsx
@@ -24,7 +24,7 @@ const Header: React.FC = () => {
         <div className="flex flex-row items-center justify-end gap-6 sm:gap-10 lg:gap-54 text-base sm:text-lg lg:text-xl text-text-color">
           <Link href="/" className="text-[#008cff] font-bold">홈</Link>
           <Link href="/compare" className="text-[#282828] font-medium">보험 비교</Link>
-          <Link href="/tips" className="text-[#282828] font-medium">보험 팁</Link>
+          <Link href="/news" className="text-[#282828] font-medium">보험 팁</Link>
           <Link href="/reviews" className="text-[#282828] font-medium">고객 리뷰</Link>
           <Link href="/faq" className="text-[#282828] font-medium">FAQ</Link>
         </div>
