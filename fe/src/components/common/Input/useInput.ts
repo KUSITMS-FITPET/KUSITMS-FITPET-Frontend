d45125057214a7626b0fdf0383f5d8104aed3ede
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-03 00:19:51 +0900

    feat: Input 컴포넌트

---
+++ fe/src/components/common/Input/useInput.ts
@@ -0,0 +1,87 @@
+'use client'
+
+import { InputHTMLAttributes, ChangeEvent, useCallback } from 'react'
+import { cn } from '@/util'
+import { useDOMRef } from '@/hooks'
+import type { ReactRef } from '@/type'
+
+export interface UseInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  ref?: ReactRef<HTMLInputElement>
+  wrapperClassName?: string
+  error?: boolean
+  startContent?: React.ReactNode
+  endContent?: React.ReactNode
+  isClearable?: boolean
+  onValueChange?: (value: string) => void
+  onClear?: () => void
+}
+
+export function useInput(props: UseInputProps) {
+  const {
+    ref,
+    error,
+    wrapperClassName,
+    startContent,
+    endContent,
+    isClearable,
+    onValueChange,
+    onClear,
+    ...otherProps
+  } = props
+
+  const domRef = useDOMRef(ref)
+
+  const handleClear = useCallback(() => {
+    onValueChange?.('')
+    onClear?.()
+  }, [onValueChange, onClear])
+
+  const handleChangeValue = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (!otherProps.disabled) {
+        otherProps.onChange?.(e)
+        onValueChange?.(e.target.value)
+      }
+    },
+    [otherProps, onValueChange],
+  )
+
+  const getBaseProps = useCallback(
+    () => ({
+      className: cn(
+        'flex items-center bg-white rounded-8 px-20 border border-[#D1D5DB]',
+        wrapperClassName,
+      ),
+    }),
+    [wrapperClassName],
+  )
+
+  const getInputProps = useCallback(
+    () => ({
+      ...otherProps,
+      ref: domRef,
+      className: cn(
+        'bg-white my-12 focus:outline-none',
+        otherProps.className,
+        error && 'border-[##EF4444]',
+      ),
+      onChange: handleChangeValue,
+    }),
+    [domRef, handleChangeValue, otherProps],
+  )
+
+  const getClearButtonProps = useCallback(
+    () => ({
+      onClick: handleClear,
+    }),
+    [handleClear],
+  )
+
+  return {
+    handleClear,
+    getBaseProps,
+    getInputProps,
+    getClearButtonProps,
+    isClearable,
+  }
+}
