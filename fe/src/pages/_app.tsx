00de16a8f4aa9d13b4fa0b4d1c8d3aa8d6e7315e
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-03 04:11:09 +0900

    fix

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import Header from '@/components/layout/Header' // Corrected import path for Header
-import Footer from '@/components/layout/Footer' // Corrected import path for Footer
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import localFont from 'next/font/local'
@@ -11,16 +9,12 @@ const Pretendard = localFont({
   weight: '45 920',
 })
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryProvider>
       <div className={Pretendard.className}>
-        <Header />
         <Component {...pageProps} />
-        <Footer />
       </div>
     </QueryProvider>
   )
 }
-
-export default MyApp
