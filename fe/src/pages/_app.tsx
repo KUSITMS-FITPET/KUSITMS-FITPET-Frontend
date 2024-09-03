e312e0aa05ebf77aabf5412aa1bad09411077d6d
karnelll <165611407+karnelll@users.noreply.github.com>
2024-09-04 00:36:35 +0900

    Update _app.tsx to include Header and Footer components

--- fe/src/pages/_app.tsx
+++ fe/src/pages/_app.tsx
@@ -1,20 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import localFont from 'next/font/local'
-import QueryProvider from '@/lib/QueryProvider'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+import QueryProvider from '@/lib/QueryProvider';
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 
 const Pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '45 920',
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryProvider>
       <div className={Pretendard.className}>
-        <Component {...pageProps} />
+        <Header />
+        <main className="min-h-screen">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
       </div>
     </QueryProvider>
-  )
+  );
 }
