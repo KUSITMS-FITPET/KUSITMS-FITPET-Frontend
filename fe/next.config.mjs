d312e53c6d10917b05f674c6f0c2c66f14ca1a85
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-04 05:34:14 +0900

    setting

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fitpetbucket.s3.us-east-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
