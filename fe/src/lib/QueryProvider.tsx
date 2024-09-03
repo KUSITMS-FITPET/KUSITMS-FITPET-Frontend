649171fbd905099eb477cbd5e545a0aac0cb24da
karnelll <165611407+karnelll@users.noreply.github.com>
2024-09-03 20:47:33 +0900

    Update QueryProvider.tsx

--- fe/src/lib/QueryProvider.tsx
+++ fe/src/lib/QueryProvider.tsx
@@ -1,25 +1,19 @@
 'use client'
 
-import { useState, ReactNode } from 'react'
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictPropsWithChildren } from '@/type'
 
-export default function QueryProvider({ children }: StrictPropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
+	@@ -10,14 +10,16 @@ export default function QueryProvider({ children }: StrictPropsWithChildren) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 쿼리의 신선도 유효 기간을 1분으로 설정
-            retry: false, // 쿼리 실패 시 재시도하지 않음
+            staleTime: 60 * 1000,
+            retry: false,
           },
         },
       }),
   )
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  )
-}
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
