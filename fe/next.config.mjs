9a9b8b998748867f97a80027dbf04a0bd5e7b23e
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-05 04:24:50 +0900

    HOTFIX

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/api/:path*`, 
+      },
+    ]
+  },
 }
 
 export default nextConfig
