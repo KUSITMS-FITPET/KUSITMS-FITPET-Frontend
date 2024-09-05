56b4d7e66ebc8b0f1bec55af1c924bba69376069
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-05 18:27:43 +0900

    fix

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*`, 
+        destination: `${process.env.API_URL || 'http://localhost:3000'}/api/:path*`,
       },
     ]
   },
