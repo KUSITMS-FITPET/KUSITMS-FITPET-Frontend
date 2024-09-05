e458cdf3e098b51202cc964ce3d01ee25273bcc7
karnelll <165611407+karnelll@users.noreply.github.com>
2024-09-06 00:11:40 +0900

    Fix Filter and ReviewItem components

--- fe/src/components/customerReview/ReviewItem.tsx
+++ fe/src/components/customerReview/ReviewItem.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
-import Image from 'next/image'
 import { getReviewById, Review } from '../../api/customereview'
-        
+
 interface ReviewItemProps {
   reviewId: number
 }
