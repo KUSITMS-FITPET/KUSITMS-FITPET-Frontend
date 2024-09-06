d5227866b4fb4b588ee1764bbb42291f0df1ca4b
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-09-07 01:58:55 +0900

    HOTFIX

--- fe/src/pages/_document.tsx
+++ fe/src/pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  ;<meta
+    httpEquiv="Content-Security-Policy"
+    content="upgrade-insecure-requests"
+  />
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className="bg-bgColor3">
         <Main />
         <NextScript />
